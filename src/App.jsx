83d5f47098dab3c4b2d2535a92a5e6b8bad9a26f
SHIV KUMAR <135263183+SHIV3710@users.noreply.github.com>
2024-03-10 16:51:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const App = () => {
         ) : (
           <>
             <Financial />
-            {/* <Commercial /> */}
+            <Commercial />
           </>
         )}
       </Graph>
